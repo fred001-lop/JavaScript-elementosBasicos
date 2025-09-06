4c1266bb6c5db4a8dabee04601b1af687f3777a1
Fredy Lopez <fredygutierrez1661@gmail.com>
2025-09-05 22:33:39 -0600

    modified files

---
+++ operadoresDeComparacion/igualdadEstrictaODebil.js
@@ -0,0 +1,34 @@
+let a = 10;
+let b = "10";
+let c = true;
+
+
+//comparaciones
+let d = a == b;
+let e = a === c;
+let f = b != c;
+let g = a !== b;
+
+//resultados
+
+console.log(`D = ${d}`);
+console.log(`E = ${e}`);
+console.log(`F = ${f}`);
+console.log(`G = ${g}`);
+
+
+
+
+
+
+
+
+/*Se te proporciona un código con tres variables: a, b y c.
+
+Tu tarea es realizar las siguientes comparaciones e imprimir los resultados:
+
+Verifica si a es vagamente igual a b y guarda el resultado en d.
+Verifica si a es estrictamente igual a c y guarda el resultado en e.
+Verifica si b no es vagamente igual a c y guarda el resultado en f.
+Verifica si a no es estrictamente igual a b y guarda el resultado en g.
+Usa las variables dadas y los operadores de comparación para completar la tarea.*/
