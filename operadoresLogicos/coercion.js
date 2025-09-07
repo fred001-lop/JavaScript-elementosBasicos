22f451d17c6c251eb5d44b4d9fe7e6f66305dfa8
Fredy Lopez <fredygutierrez1661@gmail.com>
2025-09-07 15:00:09 -0600

    add new files

---
+++ operadoresLogicos/coercion.js
@@ -0,0 +1,18 @@
+let num = 42;
+let bool = false;
+let str = "7";
+
+//operaciones
+let a = Number(num);
+let b = Number(bool);
+let c = Number(str);
+let d = num + str;
+
+
+console.log(`a = ${a}`);
+console.log(`b = ${b}`);
+console.log(`c = ${c}`);
+console.log(`d = ${d}`);
+
+
+
