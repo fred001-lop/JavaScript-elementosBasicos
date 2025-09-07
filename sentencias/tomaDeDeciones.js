c80ba560f2570c93406309a521a6c1060b278023
Fredy Lopez <fredygutierrez1661@gmail.com>
2025-09-07 17:07:20 -0600

    Add && modified files

---
+++ sentencias/tomaDeDeciones.js
@@ -0,0 +1,20 @@
+let inp = 22;
+let wind = parseInt(inp);
+let condition = "sunset";
+
+//condicion
+
+if (wind <= 8){
+    condition = "Calm";
+
+}else if (wind >= 8 && wind <= 31){
+    condition = "Breeze";
+
+}else if (wind >= 32 && wind <= 63 ){
+    condition = "Gale";
+}else{
+
+    condition = "Storm";
+}
+
+console.log(condition);
